09fd8128a8e684c8f37572253cca3cfb5a2ad329
gabivdcamargo <159277563+gabivdcamargo@users.noreply.github.com>
2024-08-14 14:37:36 -0300

    Primeiro commit

---
+++ areaCircunferencia.java
@@ -0,0 +1,16 @@
+package somanumInte;
+
+public class areaCircunferencia {
+
+	public static void main(String[] args) {
+		//	DECLARA AS VAREAVEIS
+		int raio = 4;
+		final double PI = 3.14159;
+		double area = PI * raio * raio;
+		
+		//EXIBE O RESULTADO
+		System.out.println(area);
+
+	}
+
+}
---
+++ somaNum.java
@@ -0,0 +1,34 @@
+package somanumInte;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class somaNum {
+
+	public static void main(String[] args) {
+
+		Scanner Scanner = new Scanner (System.in);
+		ArrayList<Integer> numeros = new ArrayList <>();
+
+		System.out.println("Digite números inteiros (digite '0' para finalizar):");
+
+		while (true) {
+			int numero = Scanner.nextInt();
+			if (numero == 0) {
+				break;
+			}
+			numeros.add(numero);
+		}
+		int soma = somarNumeros(numeros);
+		System.out.println("A soma de todos os números é: " + soma);
+	}
+	public static int somarNumeros (ArrayList<Integer> numeros) {
+		int soma = 0;
+		for (int numero: numeros) {
+			soma += numero;
+		}
+		return soma;
+
+	}
+
+}
